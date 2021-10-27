0b922da1f9c05ecf07cae2cea12b77fec0725d18
stu <stu@DESKTOP-14CD90K>
2021-10-27 23:18:04 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("podaj prosze ilosc liczb pierwszych ktore chcesz wypisac");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-----------------------");
+            int count;
+            int c;
+            int i = 2;
+
+            for (count = 1; count <= n; i++)
+            {
+                for (c = 2; c < i; c++)
+                {
+                    if (i % c == 0)
+                        break;
+                }
+                if (c == i)
+                {
+                    
+                    Console.WriteLine(i);
+                    ++count;
+                }
+            }
+
+        }
+
+    }
+}
